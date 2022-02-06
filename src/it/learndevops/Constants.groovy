8bfd4d1eb6f2f75addce5809fdbdc98ebf01cd5d
Guillaume Goisset <guillaume.goisset@gmail.com>
2022-02-06 18:19:50 +0100

    Initial dir structure and files

---
+++ src/it/learndevops/Constants.groovy
@@ -0,0 +1,6 @@
+package it.learndevops
+
+class Constants {
+	public static final String DEFECTDODO_URL = "http://"
+	public static final String DEFECTDOJO_CREDENTIALS_ID = "defectdojo-api-key"
+}
---
+++ vars/importScanResults.groovy
@@ -0,0 +1,5 @@
+import it.learndevops.Constants
+
+def call (Map args) {
+	echo Constants.DEFECTDOJO_URL
+}
